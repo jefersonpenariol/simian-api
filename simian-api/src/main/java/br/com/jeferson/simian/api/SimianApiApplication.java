097ec83b6cd957efaeb7ebd3ff1ecd8f1f6b2406
0153313 <0153313@host.docker.internal>
2021-03-18 19:59:46 -0300

    Simian api

---
+++ simian-api/src/main/java/br/com/jeferson/simian/api/SimianApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jeferson.simian.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimianApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimianApiApplication.class, args);
+	}
+
+}
---
+++ simian-api/src/test/java/br/com/jeferson/simian/api/SimianApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jeferson.simian.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimianApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
